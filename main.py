a51dcd2f06a0bba1c2a0ecbdab4c703ae43bb4c2
CNFLKT7 <danilatit@yandex.ru>
2021-12-07 12:36:46 +0300

    Добавил функцию отправки сообщений

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+import sys
+import datetime
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+import json
+import requests
+from requests.exceptions import HTTPError
 
-def print_hi(name):
-    print(f'Hi, {name}')
+
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = "http://localhost:5000"
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('messanger.ui', self)
+        self.pushButton1.clicked.connect(self.pushButton1_clicked)
+
+    def pushButton1_clicked(self):
+        self.SendMessage()
+
+    def SendMessage(self):
+        UserName = self.lineEdit1.text()
+        MessageText = self.lineEdit2.text()
+        TimeStamp = str(datetime.datetime.today())
+        msg = f"{{\"UserName\": \"{UserName}\", \"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        print("Отправлено сообщение: " + msg)
+        url = self.ServerAdress + "/api/Messanger"
+        data = json.loads(msg)
+        r = requests.post(url, json=data)
+
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec())
 
